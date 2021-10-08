0609493a9e7b10c0ee9c987e0a3e2eb89b3119f7
Anna <annaange55@gmail.com>
2021-10-08 23:30:20 +0300

    few bugs is fixed

--- js/index.js
+++ js/index.js
@@ -7,5 +7,6 @@ input.addEventListener('input', () => factorial(input.value));
 
 function factorial(n) {
     if (n == 1) return output.value = n;
+    if (n == 0) return output.value = 0;
     return output.value = n * factorial(n - 1);
 }
